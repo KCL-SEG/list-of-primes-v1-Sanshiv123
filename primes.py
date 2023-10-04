c23f1e19aa0c37f908b2e267416be09184156e22
Sanchusri Kavitha Babu <sanchusri.kavitha_babu@kcl.ac.uk>
2023-10-05 00:00:17 +0100

    solve exercise 1.3

--- primes.py
+++ primes.py
@@ -3,4 +3,12 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+    for num in range(1, 500):
+        if num>1:
+            for i in range(2, num):
+                if(num%i)==0:
+                    break
+            else:
+                list.append(num)
+    return list[0:number_of_primes]
+print(f'first 10 prime numbers are {primes(10)}')
